d9caf914d89a5ed822d3f12449822b6d01d31d57
Ubuntu <ubuntu@ip-172-31-91-250.ec2.internal>
2026-03-19 19:01:11 +0000

    synth

--- design_top/build/src_post_encryption/counter.v
+++ design_top/build/src_post_encryption/counter.v
@@ -8,25 +8,25 @@
 `pragma protect begin_toolblock
 `pragma protect rights_digest_method="sha256"
 `pragma protect key_keyowner = "Xilinx", key_keyname= "xilinxt_2023_11", key_method = "rsa", key_block
-veyaVc6LPiGn+4ifCg5SPNIgyLwdSTv5oAKXqnx8gQrbyd7Hk9WvFsU1Fu5sXoWkcf908VGvBFPx
-kaG9Ww8aPnHoR8uLfu8PLQ7Z+jJFuewcjTz2dinOhE4giyS79xqJvybYa1hzkkzYH0UuP+G3ZMfU
-ex1gWSf9KbflIOruTWSpSpPj3jpb6CtXGajLjG6B8Cx+FICqm5BL3tI2BPCBc7YNMaeDRL8h13ib
-zrP1atR/DRrT6wOAHZ63LR9hKMTFkXWkm0axqzorX7zIi+oNL53skahIh15dCgAkZWSSYqnxIP2S
-gW82WmGMZBkKIe7nEKzc/IC8sxcHyFzty8s81Q==
+PcUPDayyNOBVeJuOrj8YCjEajXPglkfzfAw2wSfDE6zqCX/Hf5pXKsBSSBOpRm0OHwU7RB4GjenR
+kTYbTVABr7OzOUnLVrKaePvlg8x55Uv/VY8fN29N4OE2cFdd2sGT+RwFEjnCuy5e29OQZo2RApYO
+JngthviCNKTFcnSF143p4tKlqt9eM/urxcm01voDtxvpUVsVQLt47oMZIN+xg1apNchJ7k8nBva5
+5pjA64RxyOHnwC1bgv4uMtgaYsC9YWYaEFFFK6xj/qBbXXoRQoZyvaz5d8hkER52wGS/RNUU+BXp
+dt45cSJqKIuI2r6YYjVMjmQIvHhj74UoQhszAQ==
 
 `pragma protect control xilinx_configuration_visible = "false"
 `pragma protect control xilinx_enable_modification = "false"
 `pragma protect control xilinx_enable_probing = "false"
-`pragma protect end_toolblock="x7tPupxjTcknM5dJz9vnndhRQQ4bByu5lgPlsznNg68="
+`pragma protect end_toolblock="Wkfmz6l7dgkerDqd8GVxATScol7JRlz9NH0RpVLCU2w="
 `pragma protect data_method = "AES128-CBC"
 `pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 448)
 `pragma protect data_block
-jBbXiZe95ChqGvm+KC+kEpRbgjnkg/z/1mzHJfQONq+VSW3QjjCtKYKC+Uah/HkWMdyPk+nDdwHH
-rnFsyBROuCisaB2phQRsUig5Sv+wstyddZqJ+SNPZ9FhDMMUuWMvYLXG4Aoh8w7xFq6vPN54Z7LY
-ED5SL8gB94qVFDqN0da9u5NpY29SRkVBNBvvJEAEMq4sJ4pG8R1GtqIkUxoYvpxL+vrhu7ID4a2V
-xPwTtfTl06kDI7CI5lmftwgeVxyX/hJ7sc9xKv+x9tNlW3fNWsNDw5OxoU6SEO+DBAhVgknyxIF8
-j6CsNE8E/WEI5VvoU3unnebR/nerzaEkPdKlixH/TqBqsyEr7I5zeex9I54gsXRhOMek0y/C9QEy
-GrL6WADHZ1/w++58Xa3ZNhT6KtCQpfJ0EUsN+v3eSw0kpRuqwtJj8vljfyTWMEuekPE7Kqg05rG1
-ZeOWpBmIukNWVrgLXU3tBrRBnP+GQuNBC0Y4XNUqYD8gsZpasD/5M0ITYdSmF7ntjRvF5EKAodnY
-HxDPis/3vw3fCu/S8BuXyd/wp3gf7M+JsL85hRZ6lfcQRiy90wvnE347nV0e8m+uKQ==
+SYTcZD3wTjfAKPKzbirjpsQBTOmMDqOgHKrev3rLEzNrscHOoXXP5qTyg5nVyiqtiBli7JI2jrAu
+LbS8GmdUfZIMx0IYTa+IpabhsgYmCn70l6pkwwy9Y6MNg1hFSPVAMkkFTNBXxmBwCWWV6O9AABsh
+UErluOUDpwXoJ01fZsxROnsH5eeEhcqOpmWoegH0q9OKrtNfV/LaK9ifDgW1qAYiVlm4nTgGUNWj
+x3UmuFxkLYQ/ytJAzcbbbWWuxBttpAoKXtMqPqxTur15gQ5YoFvuvBo7iq45VNOzW+f+F1efBmjz
+vsnSkzGM2ucggTApXyS9LbgnclriZZri6WMA4y+zUzcGXQimy3QiirAal9oupnaDpmHX9aZPb5nU
+H2/Q6O0TNSfumWjHbSLQ/TbdvVacF/zNtXZ8nNT8N7zJ0sY5hfbCxxNnbY+cYBkGy7kXboElIANx
+Ri+jkRSpgYtdm1gPt9J1Sr/7HcJsmClp/MPzKgDuGSyfp71ntQ6INDSIrfSvoSETb4KZM+EyLKto
+a/4XVrHoQfEGMF4K9ZHSu53wA88VtuqBWmO4fKEiGrGzim1dsAvL7xpAm90HXYpT4Q==
 `pragma protect end_protected
